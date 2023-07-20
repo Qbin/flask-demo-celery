47b9bf0d2db93cb8385a2e5bc813c8ba04de7e0c
qinbinbin <qinbinbin@360.cn>
2023-07-20 16:08:47 +0800

    feat(text_cluster): 增加测试脚本
    1. 增加test_cluster脚本，用于测试万条数据的性能

---
+++ app/text_cluster/test_cluster.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2023/7/20 11:40
+# @File    : test_cluster.py
+from algorithms.kmeans import KMEANS
+from app.text_cluster.analyzed_data_model import AnalyzedData
+
+from mongoengine import connect
+
+from common.time_cost import calculate_runtime
+
+connect(db='prophet_k8s_test', host='mongodb://mongo:73c74b9461f00453@10.220.139.135:7830',
+        authentication_source='admin')
+
+from concurrent.futures import ThreadPoolExecutor
+
+
+def join_analyzed_data(i):
+    return ' '.join(i.analyzed_data)
+
+
+@calculate_runtime
+def main():
+    data_indexes = list(range(1000))
+    data_indexes = ["10000002", "10000003", "10000004", "10000005", "10000006", "10000007", "10000008", "10000009",
+                    "10000010", "10000011", "10000012", "10000013", "10000014", "10000015", "10000016", "10000017",
+                    "10000018", "10000019", "10000020", "10000021", "10000022", "10000023", "10000024", "10000025",
+                    "10000026", "10000027", "10000028", "10000029", "10000030", "10000031", "10000032", "10000033",
+                    "10000034", "10000035"]
+    texts = AnalyzedData.batch_find_by_ids(data_indexes)
+    corpus = [' '.join(i.analyzed_data) for i in texts]
+    # corpus = list(map(lambda i: ' '.join(i.analyzed_data), texts))
+
+    # 创建线程池
+    # with ThreadPoolExecutor() as executor:
+    #     # 使用线程池并行处理任务
+    #     corpus = list(executor.map(join_analyzed_data, texts))
+
+    print(len(corpus))
+    kmeans = KMEANS(corpus, num_clusters=3)
+    model = kmeans.train()
+    # model_id = self.save_model(model, cluster_params)
+
+
+if __name__ == '__main__':
+    main()
